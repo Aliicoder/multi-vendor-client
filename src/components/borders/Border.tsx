2e6b0d9b3189e872bf8f940ecf08a86bddfe5890
ali_fahmi <k.c.o.c.3000@gmail.com>
2025-05-10 20:53:02 +0530

    save : save new changes 001

--- src/components/borders/Border.tsx
+++ src/components/borders/Border.tsx
@@ -1,20 +1,23 @@
-import useSquircle from '@/hooks/useSquircle'
-import { Squircle } from 'corner-smoothing'
-import { PropsWithChildren } from 'react'
+import useSquircle from "@/hooks/useSquircle";
+import { Squircle } from "corner-smoothing";
+import { PropsWithChildren } from "react";
 interface Styles extends PropsWithChildren {
-  className?: string
-  topClassName?: string
-  onClick?:() => void
+  className?: string;
+  topClassName?: string;
+  onClick?: () => void;
 }
-function Border({className,topClassName,onClick,children}:Styles) {
-  const cornerRadius = useSquircle()
+function Border({ className, topClassName, onClick, children }: Styles) {
+  const cornerRadius = useSquircle();
   return (
-      <div onClick={onClick} className={`${topClassName} drop-shadow-sm`} >
-        <Squircle cornerRadius={cornerRadius} className={className}>
-          {children}
-        </Squircle>
-      </div>
-  )
+    <div
+      onClick={onClick}
+      className={`${topClassName} drop-shadow-sm shadow-sm`}
+    >
+      <Squircle cornerRadius={cornerRadius} className={className}>
+        {children}
+      </Squircle>
+    </div>
+  );
 }
 
-export default Border
+export default Border;
